057ffdcfe2e2c330d9de8cef925da4e9b93e24eb
jondoetsch27 <jd@enfuse.io>
2024-03-12 12:57:31 -0700

    NORMALIZE. TINY. REFACTORS.

--- src/get_path.py
+++ src/get_path.py
@@ -1,61 +1,48 @@
 import argparse
-import matplotlib.pyplot as plt
 import inference as inference
 import utils.bw_map_data_generator as bw_map_data_generator
 
 # input params
 def parse_args():
-    parser = argparse.ArgumentParser(description="Path Planning Visualization Script")
-    # Args to generate scaled map and start and end maps
-    parser.add_argument("--image_path",
-                        type=str,
-                        help="Path to unscaled map image"
-                        )
-    parser.add_argument("--target_size_x",
-                        type=int,
-                        default=64,
-                        help="Dictates size x(width) of image"
-                        )
-    parser.add_argument("--target_size_y",
-                        type=int,
-                        default=64,
-                        help="Dictates size y(height) of image"
-                        )
-    parser.add_argument("--start_point_x",
-                        type=int,
-                        help="Starting x point for start image"
-                        )
-    parser.add_argument("--start_point_y",
-                        type=int,
-                        help="Starting y point for start image"
-                        )
-    parser.add_argument("--goal_point_x",
-                        type=int,
-                        help="Goal x point for goal image"
-                        )
-    parser.add_argument("--goal_point_y",
-                        type=int,
-                        help="Goal y point for goal image"
-                        )
-
+    #  Args to generate scaled map and start and end maps
+    parser = argparse.ArgumentParser(description = "Path Planning Visualization Script")
+    parser.add_argument("--image_path", type = str, help = "Path to unscaled map image")
+    parser.add_argument("--target_size_x", type = int, default = 64, help = "Dictates size x(width) of image")
+    parser.add_argument("--target_size_y", type = int, default = 64, help = "Dictates size y(height) of image")
+    parser.add_argument("--start_point_x", type = int, help = "Starting x point for start image")
+    parser.add_argument("--start_point_y", type = int, help = "Starting y point for start image")
+    parser.add_argument("--goal_point_x", type = int, help = "Goal x point for goal image")
+    parser.add_argument("--goal_point_y", type = int, help = "Goal y point for goal image")
     args = parser.parse_args()
-
     return args
 
 def main(args):
     start_filename = 'start_map'
     goal_filename = 'goal_map'
     # create resized map
-    bw_map_data_generator.resize_and_pad(args.image_path, args.target_size_x, args.target_size_y)
+    bw_map_data_generator.resize_and_pad(
+        image_path = args.image_path, 
+        target_size_x = args.target_size_x, 
+        target_size_y = args.target_size_y
+    )
     # create start and goal images
-    bw_map_data_generator.create_start_or_goal_image(args.start_point_x, args.start_point_y, 
-                                                     start_filename, args.target_size_x, args.target_size_y)
-    bw_map_data_generator.create_start_or_goal_image(args.goal_point_x, args.goal_point_y, 
-                                                     goal_filename, args.target_size_x, args.target_size_y)
-    # infer path 
+    bw_map_data_generator.create_start_or_goal_image(
+        x_point = args.start_point_x, 
+        y_point = args.start_point_y, 
+        filename = start_filename, 
+        target_size_x = args.target_size_x, 
+        target_size_y = args.target_size_y
+    )
+    bw_map_data_generator.create_start_or_goal_image(
+        x_point = args.goal_point_x, 
+        y_point = args.goal_point_y, 
+        filename = goal_filename, 
+        target_size_x = args.target_size_x, 
+        target_size_y = args.target_size_y
+    )
+    # infer path (wayfinding)
     inference.infer_path()
 
 if __name__ == "__main__":
     args = parse_args()
-
     main(args)
