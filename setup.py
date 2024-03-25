b13e11a9f599b406112822ba20356e6c13425729
jondoetsch27 <jd@enfuse.io>
2024-03-25 15:39:49 -0700

    version 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 # Package metadata
 NAME = 'trans_path_enfuse'
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 DESCRIPTION = 'Enfuse implementation of TransPath model from AIRI'
 AUTHOR = 'Enfuse.io'
 EMAIL = 'jd@enfuse.io'
@@ -11,7 +11,7 @@ LICENSE = 'MIT'
 PYTHON_VERSION = '>=3.11.0'
 
 # Long description from README.md
-with open('README.md', 'r', encoding='utf-8') as f:
+with open('README.md', 'r', encoding = 'utf-8') as f:
     long_description = 'you do not need the whole README here'
 
 # Required packages
@@ -19,19 +19,19 @@ with open('requirements.txt', 'r') as f:
     required_packages = f.read().splitlines()
 
 setup(
-    name=NAME,
-    version=VERSION,
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author=AUTHOR,
-    author_email=EMAIL,
-    url=URL,
-    license=LICENSE,
-    python_requires=PYTHON_VERSION,
-    packages=find_packages(),
-    install_requires=required_packages,
-    classifiers=[
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    author = AUTHOR,
+    author_email = EMAIL,
+    url = URL,
+    license = LICENSE,
+    python_requires = PYTHON_VERSION,
+    packages = find_packages(),
+    install_requires = required_packages,
+    classifiers = [
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3.11',
